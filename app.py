d6bd3928233996896b52a5ce1884cc266168563b
Pramodh Mazumdar <pmazumdar@rivian.com>
2018-08-07 11:57:33 -0700

    init commit with test and docker

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import os
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    try:
+        visits = 1
+    except Exception:
+        visits = "<i>cannot connect to Redis, counter disabled</i>"
+
+    html = "<h3>Hello {name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>Visits:</b> {visits}"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
---
+++ test_app.py
@@ -0,0 +1,6 @@
+def be_truthful():
+    return True
+
+
+def test_truthful():
+    assert be_truthful() == True
